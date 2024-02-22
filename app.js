b7a4af07b396ae9598d2e90872c65da8052bdfac
Bernardo Enrique Albino Martinez <enriquealbinomartinez@gmail.com>
2024-02-21 20:22:12 -0600

    ajustes a pokemon

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ let pokemonList = '';
 // val puede ser id o name
 async function cargaPokemon(val) {
     const url = `${apiUrl}${val}`;
-    console.log('soy la url lista', url);
     try {
          await fetch(url)
         .then(res => res.json())
@@ -23,6 +22,7 @@ function render(pokemon) {
     limpiaContent();
     pokemonList = pokemon.name;
     const referencia = document.querySelector(".contenedor-pokemons");
+    referencia.classList.add("hidden");
     // referencia.classList.add("card");
     const titulo = document.createElement("h3");
     titulo.innerHTML = `El pokemon es: ${pokemon.name}`;
@@ -45,6 +45,36 @@ function render(pokemon) {
     referencia.append(idPokemon);
     referencia.appendChild(botonAgregar);
     referencia.appendChild(botonHabilidades);
+    referencia.classList.remove("hidden");
+
+    botonAgregar.addEventListener('click', () => {
+        agregaPokemon();
+    })
+    botonHabilidades.addEventListener('click', () => {
+        renderHabilidades(pokemon);
+    })
+ 
+}
+function renderHabilidades(pokemon) {
+    console.log('en render habilidades', pokemon);
+    const referencia = document.querySelector(".habilidades-extra");
+    referencia.classList.add('style-habilidad')
+    referencia.innerHTML = '';
+    if (!referencia.hasChildNodes()) {
+        const title = document.createElement("h3");
+        title.textContent = 'Habilidades Extra';
+
+        const pesoPokemon = document.createElement("h6");
+        pesoPokemon.textContent = ` El peso de ${pokemon.name} es: ${pokemon.weight}`;
+
+        const alturaPokemon = document.createElement("h6");
+        alturaPokemon.textContent = ` La altura de ${pokemon.name} es: ${pokemon.height}`;
+
+        referencia.appendChild(title);
+        referencia.appendChild(pesoPokemon);
+        referencia.appendChild(alturaPokemon);
+    }
+
 }
 document.addEventListener('DOMContentLoaded', () => {
     const botonBuscar = document.getElementById('buscar-pokemon');
@@ -53,44 +83,55 @@ document.addEventListener('DOMContentLoaded', () => {
         // if ( typeof pokemonNameId === number) {
 
         // }
-        console.log(typeof pokemonNameId);
+        
         cargaPokemon(pokemonNameId);
-        console.log('soy el parametro de busqueda', pokemonNameId);
+        const referencia = document.querySelector(".habilidades-extra");
+        referencia.innerHTML = '';
+        // console.log('soy el parametro de busqueda', pokemonNameId);
     })
 } )
 
- document.addEventListener('DOMContentLoaded', () => {
-    const referencia = document.querySelector(".contenedor-pokemons");
-    //  const botonBuscar = document.getElementById('agregar-pokemon');
-     referencia.addEventListener('click', () => {
-        console.log('SI DETECTO');
-        agregaPokemon();
-     })
- } )
+//  document.addEventListener('DOMContentLoaded', () => {
+//     const referencia = document.querySelector(".contenedor-pokemons");
+//     //  const botonBuscar = document.getElementById('agregar-pokemon');
+//      referencia.addEventListener('click', () => {
+//         console.log('SI DETECTO');
+//         agregaPokemon();
+//      })
+//  } )
 
- document.addEventListener('DOMContentLoaded', () => {
-    const referencia = document.querySelector(".contenedor-pokemons");
-    //  const botonBuscar = document.getElementById('agregar-pokemon');
-     referencia.addEventListener('click', () => {
-        console.log('SI DETECTO');
-        agregaPokemon();
-     })
- } )
+//  document.addEventListener('DOMContentLoaded', () => {
+//     const referencia = document.querySelector(".contenedor-pokemons");
+//     //  const botonBuscar = document.getElementById('agregar-pokemon');
+//      referencia.addEventListener('click', () => {
+//         console.log('SI DETECTO');
+//         agregaPokemon();
+//      })
+//  } )
 // funcion para agregar a la lista
 function agregaPokemon() {
     console.log('soy el agregador',pokemonList);
-    listaPokemon.push(pokemonList);
-    const referencia = document.querySelector(".listado-agregados");
-    referencia.innerHTML = '';
-    const ul = document.createElement("ul");
-    listaPokemon.forEach(pokemon => {
-        const li = document.createElement("li");
-        li.textContent = pokemon;
-        ul.appendChild(li)
-    })
-    referencia.appendChild(ul);
+    if ( !listaPokemon.includes(pokemonList)) {
+        listaPokemon.push(pokemonList);
+        const referencia = document.querySelector(".listado-agregados");
+        referencia.classList.add('listado-pokedex')
+        referencia.innerHTML = '';
+        const ul = document.createElement("ul");
+        listaPokemon.forEach(pokemon => {
+            const li = document.createElement("li");
+            li.textContent = pokemon;
+            ul.appendChild(li)
+        })
+        referencia.appendChild(ul);
+    } else {
+        alert(`${pokemonList} ya se encuentra en tu pokedex :p`)
+        // console.log('ya esta tu pokemon');
+    }
+    
 
 }
+
+
 // funcion para limpiar el div
 function limpiaContent() {
     const referencia = document.querySelector(".contenedor-pokemons");
